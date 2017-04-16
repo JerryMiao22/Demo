a6980fb48b310f15050aa7419e3418fdcc793aca
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2017-04-16 09:09:38 -0700

    Update levels.js

--- levels.js
+++ levels.js
@@ -14,26 +14,38 @@ const LEVELS = [
         ],
         platforms : [
             // width, height, x, y, img
-            new Platform (640, 250, 0, 480 - 250, platformL1P1)
+            new Platform (640, 250, 0, 480 - 250, L0Floor)
         ]
     },
 
     // Level #1
     {
         // width, height, color, x, y
-        player : new Player(PLAYER_SIZE, PLAYER_SIZE, "gray", 20, 300), 
+        player : new Player(PLAYER_SIZE, PLAYER_SIZE, "gray", 80, 350), 
         doors : [
             // width, height, x, y, color
-            new Door (55, 65, 525, 60, doorImg)
+            new Door (55, 65, 525, 60, doorImg),
+            new Door (55, 65, 20, 15, doorImg)
         ],
         platforms : [
             // width, height, x, y, color
-            new Platform (640, 50, 0, 480 - 50, "black"),
-            new Platform (100, 30, 475, 350, "black"),
-            new Platform (110, 30, 300, 280, "black"),
-            new Platform (150, 30, 60, 250, "black"),
-            new Platform (120, 30, 270, 150, "black"),
-            new Platform (200, 30, 640 - 200, 125, "black")
+            
+            // Floor
+            new Platform (640, 50, 0, 480 - 50, L1Floor),
+            // Entrance Door
+            new Platform (55, 65, 20, 480 - 115, entranceImg),
+            // Platform 1
+            new Platform (100, 30, 475, 350, L1P1),
+            // Platform 2
+            new Platform (110, 30, 300, 280, L1P1),
+            // Platform 3
+            new Platform (150, 30, 60, 250, L1P1),
+            // Platform for Second Door
+            new Platform (140, 30, 0, 80, L1P1),
+            // Platform 4
+            new Platform (120, 30, 270, 150, L1P1),
+            // Platform 5
+            new Platform (200, 30, 640 - 200, 125, L1P1)
         ]
     },
     
@@ -48,10 +60,10 @@ const LEVELS = [
         platforms : [
             // width, height, x, y, color
             //new Platform (400, 300, 200, 200, "brown"),
-            new Platform (640 / 3, 120, 0, 480 - 120, "black"),
-            new Platform (640 / 6, 200, 640 / 3, 480 - 200, "black"),
-            new Platform (640 / 6, 280, 640 / 3 + 640 / 6, 480 - 280, "black"),
-            new Platform (640 / 3, 120, 640 - 640 / 3, 480 - 120, "black")
+            new Platform (640 / 3, 120, 0, 480 - 120, testImg),
+            new Platform (640 / 6, 200, 640 / 3, 480 - 200, testImg),
+            new Platform (640 / 6, 280, 640 / 3 + 640 / 6, 480 - 280, testImg),
+            new Platform (640 / 3, 120, 640 - 640 / 3, 480 - 120, testImg)
         ]
     },
     
@@ -66,12 +78,12 @@ const LEVELS = [
         platforms : [
             // width, height, x, y, color
             //new Platform (400, 300, 200, 200, "brown"),
-            new Platform (640 / 4, 120, 0, 480 - 120, "black"),
-            new Platform (640 / 2, 435, 640 / 4, 480 - 435, "black"),
+            new Platform (640 / 4, 120, 0, 480 - 120, testImg),
+            new Platform (640 / 2, 435, 640 / 4, 480 - 435, testImg),
             new Platform (30, 40, 640 / 4 - 30, 480 - 225, "red"),
             new Platform (30, 40, 0, 480 - 330, "red"),
             new Platform (30, 40, 640 / 4 - 30, 480 - 435, "red"),
-            new Platform (640 / 4, 120, 640 - 640 / 4, 480 - 120, "black")
+            new Platform (640 / 4, 120, 640 - 640 / 4, 480 - 120, testImg)
         ]
     },
     
@@ -86,12 +98,12 @@ const LEVELS = [
         platforms : [
             // width, height, x, y, color
             //new Platform (400, 300, 200, 200, "brown"),
-            new Platform (640 / 4, 120, 0, 480 - 120, "black"),
-            new Platform (640 / 2, 435, 640 / 4, 480 - 435, "black"),
+            new Platform (640 / 4, 120, 0, 480 - 120, testImg),
+            new Platform (640 / 2, 435, 640 / 4, 480 - 435, testImg),
             new Platform (30, 40, 640 / 4 - 30, 480 - 225, "red"),
             new Platform (30, 40, 0, 480 - 330, "red"),
             new Platform (30, 40, 640 / 4 - 30, 480 - 435, "red"),
-            new Platform (640 / 4, 120, 640 - 640 / 4, 480 - 120, "black")
+            new Platform (640 / 4, 120, 640 - 640 / 4, 480 - 120, testImg)
         ]
     }
 ];
