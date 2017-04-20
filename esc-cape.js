c1a78027c9237f37151914ea6806b8868c6ab1f3
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2017-04-20 15:44:02 -0700

    Update esc-cape.js

--- esc-cape.js
+++ esc-cape.js
@@ -17,47 +17,6 @@ var doors;
 var spikes;
 var currentLevel;
 
-var playerImg = new Image();
-playerImg.src = "imgs/testplayer1.png";
-
-var testImg = new Image();
-testImg.src = "imgs/black.png";
-
-var doorImg = new Image();
-doorImg.src = "imgs/doorclosed2.png";
-
-var entranceImg = new Image();
-entranceImg.src = "imgs/dooropen2.png";
-
-var L0Floor = new Image();
-L0Floor.src = "imgs/L0-Floor.png";
-
-var L1Floor = new Image();
-L1Floor.src = "imgs/L1-Floor.png";
-
-var L1BG = new Image();
-L1BG.src = "imgs/L1BG.png";
-
-var L1P1 = new Image();
-L1P1.src = "imgs/L1P1.png";
-
-// Spike pointing Up
-var L5Spike1 = new Image();
-L5Spike1.src = "imgs/L5Spike1.png";
-
-// Spike pointing Left
-var L5Spike2 = new Image();
-L5Spike2.src = "imgs/L5Spike2.png";
-
-// Spike pointing Down
-var L5Spike3 = new Image();
-L5Spike3.src = "imgs/L5Spike3.png";
-
-// Spike pointing Right
-var L5Spike4 = new Image();
-L5Spike4.src = "imgs/L5Spike4.png";
-
-
 // Game object for drawing on the canvas
 var game = {
     canvas : document.createElement("canvas"),
