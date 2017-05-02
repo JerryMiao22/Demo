6e8fd3273bfb0c7b4742b7d3b07fea635e7e0dbc
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2017-05-01 21:30:06 -0700

    Update esc-cape.js

--- esc-cape.js
+++ esc-cape.js
@@ -375,7 +375,7 @@ function update() {
 
 // Starts a new game from scratch
 function init() {
-    currentLevel = 0;
+    currentLevel = 4;
     game.start(); // canvas not created until this function is called
     loadLevel(LEVELS[currentLevel]);
 } // end init
