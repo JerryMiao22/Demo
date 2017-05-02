caafc7da51e62687d6dd20af97657be3857b4bf4
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2017-05-02 07:20:17 -0700

    Update images.js

--- images.js
+++ images.js
@@ -1,5 +1,5 @@
 var playerImg = new Image();
-playerImg.src = "imgs/testplayer1.png";
+playerImg.src = "";
 
 var testImg = new Image();
 testImg.src = "imgs/black.png";
@@ -26,13 +26,13 @@ var L0BG = new Image();
 L0BG.src = "imgs/L0-BG.png";
 
 var L1Floor = new Image();
-L1Floor.src = "imgs/L1-Floor.png";
+L1Floor.src = "";
 
 var L1BG = new Image();
 L1BG.src = "imgs/L1-BG.png";
 
-var L1P1 = new Image();
-L1P1.src = "imgs/L1P1.png";
+var L2Floor = new Image();
+L2Floor.src = "imgs/L2-Floor.png";
 
 var L2BG = new Image();
 L2BG.src = "imgs/L2-BG.png";
@@ -40,51 +40,57 @@ L2BG.src = "imgs/L2-BG.png";
 var L2P1 = new Image();
 L2P1.src = "imgs/L2P1.png";
 
-var L2P2 = new Image();
-L2P2.src = "imgs/L2P2.png";
-
-var L2Spike1 = new Image();
-L2Spike1.src = "imgs/L2Spike1.png";
-
 var L3BG = new Image();
-L3BG.src = "";
+L3BG.src = "imgs/L3-BG.png";
+
+var L3P1 = new Image();
+L3P1.src = "imgs/L3P1.png";
+
+var L3P2 = new Image();
+L3P2.src = "imgs/L3P2.png";
+
+var L3Spike1 = new Image();
+L3Spike1.src = "imgs/L3Spike1.png";
 
 var L4BG = new Image();
-L4BG.src = "imgs/L4-BG.png";
+L4BG.src = "";
 
 var L5BG = new Image();
-L5BG.src = "";
-
-// Spike pointing Up
-var L5Spike1 = new Image();
-L5Spike1.src = "imgs/L5Spike1.png";
-
-// Spike pointing Left
-var L5Spike2 = new Image();
-L5Spike2.src = "imgs/L5Spike2.png";
-
-// Spike pointing Down
-var L5Spike3 = new Image();
-L5Spike3.src = "imgs/L5Spike3.png";
-
-// Spike pointing Right
-var L5Spike4 = new Image();
-L5Spike4.src = "imgs/L5Spike4.png";
+L5BG.src = "imgs/L5-BG.png";
 
 var L6BG = new Image();
 L6BG.src = "";
 
-var L6SpikeLeft = new Image();
-L6SpikeLeft.src = "imgs/L6SpikeLeft.png";
+// Spike pointing Up
+var L6Spike1 = new Image();
+L6Spike1.src = "imgs/L6Spike1.png";
+
+// Spike pointing Left
+var L6Spike2 = new Image();
+L6Spike2.src = "imgs/L6Spike2.png";
+
+// Spike pointing Down
+var L6Spike3 = new Image();
+L6Spike3.src = "imgs/L6Spike3.png";
+
+// Spike pointing Right
+var L6Spike4 = new Image();
+L6Spike4.src = "imgs/L6Spike4.png";
 
 var L7BG = new Image();
-L7BG.src = "imgs/L7-BG.png";
+L7BG.src = "";
+
+var L7SpikeLeft = new Image();
+L7SpikeLeft.src = "imgs/L7SpikeLeft.png";
 
 var L8BG = new Image();
-L8BG.src = "imgs/testBG.png";
+L8BG.src = "imgs/L8-BG.png";
 
 var L9BG = new Image();
-L9BG.src = "";
+L9BG.src = "imgs/testBG.png";
+
+//var L10BG = new Image();
+//L10BG.src = "";
 
 var lava = new Image();
 lava.src = "imgs/lava.gif";
