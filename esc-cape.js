3ddbd8a8b14c9f992848b560fdf1852f57b70634
missionbit <missionbit@21a-MissionBit.local>
2019-04-25 17:42:23 -0700

    new level

--- esc-cape.js
+++ esc-cape.js
@@ -30,9 +30,10 @@ var game = {
     
     // Clears canvas
     clear : function () {
+        const imageName = ('L' + currentLevel + 'BG')
 		this.context.globalAlpha = 1;
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.context.drawImage(eval('L' + currentLevel + 'BG'), 0, 0, this.canvas.width, this.canvas.height);
+		this.context.drawImage(eval(imageName), 0, 0, this.canvas.width, this.canvas.height);
 
     }
 }; // End Game object
--- levels.js
+++ levels.js
@@ -129,6 +129,130 @@ const LEVELS = [
             new Spike (395, 25, 30, 75, Transparent),
 		]
         
-    }
-];
+    },
 
+{ 
+    // level #4 
+ 
+     // width, height, x, y
+      playerX : 70,
+      playerY : 360,
+      doors : [ // width, height, x, y, color
+        new Door (55, 65, 590, 25, doorImg),
+        new Door (55, 65, 0, 20, doorImg)
+      ],
+    platforms : [
+        // width, height, x, y, img
+        new Platform (640, 250, 0, 480-120 , Transparent), 
+        // Platform 1
+        new Platform (500, 30, 550, 90, L2P1),
+        // Platform 2
+        new Platform (30, 30, 300, 280, L2P1),
+        // Platform 3
+        new Platform (80, 30, 0, 80, L2P1),
+        // Platform 4
+        new Platform (30, 30, 250, 200, L2P1),
+        // Platform 5
+        new Platform (30, 30, 150, 100, L2P1),
+        // Platform 6
+        new Platform (30, 30, 150, 300, L2P1),
+        // Platform 7
+        new Platform (30, 30, 200, 500, L2P1),
+        // Platform 8
+        new Platform (30, 30, 310, 20, L2P1),
+        // Platform 9
+        new Platform (30, 30, 420, 210, L2P1),
+        // Platform 10
+        new Platform (30, 30, 300, 150, L2P1),
+        // Platform 11
+        new Platform (30, 30, 350, 230, L2P1),
+        // Platform 12
+        new Platform (30, 30, 450, 125, L2P1),
+        // Platform 13
+        new Platform (30, 30, 500, 250, L2P1),
+        // Platform 14
+        new Platform (30, 30, 100, 200, L2P1),
+        
+        
+
+
+    ],
+
+    spikes : [
+       
+    ],
+
+    
+
+
+
+ },
+
+ // Level #6- THE END (so far)
+ {
+    // width, height, color, x, y
+    playerX : 70,
+    playerY : 700,
+    doors : [
+        // width, height, x, y, color
+        new Door (0, 0, 700, 700, doorImg)
+    ],
+    platforms : [
+        // width, height, x, y, img
+        new Platform (0, 0, 700, 700, testImg),
+         // Platform 1
+        new Platform (20, 100, 100, 200, L2P1),
+         // Platform 2
+         new Platform (100, 20, 60, 200, L2P1),
+         // Platform 3
+        new Platform (20, 100, 170, 200, L2P1),
+         // Platform 4
+         new Platform (50, 20, 190, 230, L2P1),
+        // Platform 5
+        new Platform (20, 100, 225, 200, L2P1),
+        // Platform 6
+        new Platform (20, 100, 260, 200, L2P1),
+         // Platform 7
+         new Platform (50, 20, 270, 200, L2P1),
+         // Platform 8
+         new Platform (50, 20, 270, 245, L2P1),
+          // Platform 9
+          new Platform (50, 20, 270, 280, L2P1),
+           // Platform 10
+        new Platform (20, 100, 380, 200, L2P1),
+          // Platform 11
+         new Platform (50, 20, 400, 200, L2P1),
+         // Platform 12
+         new Platform (50, 20, 400, 245, L2P1),
+          // Platform 13
+          new Platform (50, 20, 400, 280, L2P1),
+           // Platform 14
+        new Platform (20, 100, 470, 200, L2P1),
+         // Platform 15
+         new Platform (30, 30, 470, 200, L2P1),
+          // Platform 16
+        new Platform (30, 30, 490, 227, L2P1),
+         // Platform 17
+         new Platform (30, 30, 500, 240, L2P1),
+          // Platform 18
+          new Platform (30, 30, 565, 210, L2P1),
+          // Platform 19
+          new Platform (30, 30, 580, 235, L2P1),
+           // Platform 18
+           new Platform (30, 30, 565, 260, L2P1),
+
+
+        // Platform 20
+         new Platform (20, 100, 525, 200, L2P1),
+          // Platform 21
+          new Platform (20, 100, 550, 200, L2P1),
+
+
+        
+    ],
+    spikes : [
+        new Spike (0, 0, 320, 240, testImg)
+    ]
+
+    },
+];
